ebd09c344e8abcf34a3654b22dd8b30b172f0a1a
oguzhan <oguzhan.akbolat@turnateknoloji.com>
2022-02-16 10:20:38 +0300

    https hatası

--- constants/axios.js
+++ constants/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'http://gateway.marvel.com';
+export const baseURL = 'https://gateway.marvel.com';
 export default axios.create({ baseURL });
 export const hash = '4cb9e6da3fde269c1fd7010e28a48167';
 export const publicKey = 'fd6a3429f585519be439a72203152a1a';
